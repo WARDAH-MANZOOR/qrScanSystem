2360f1ebbc30e9424f5246b627063414ab2b2286
Muhammad Owais <m.owais1045@gmail.com>
2024-09-26 18:50:53 +0500

    made global api response

--- src/controller/paymentGateway/jazzCash.ts
+++ src/controller/paymentGateway/jazzCash.ts
@@ -1,6 +1,7 @@
 // src/controllers/paymentController.ts
 import { Request, Response, NextFunction } from "express";
-import { jazzCashService } from "services/index.js"; // Import service layer
+import { jazzCashService } from "services/index.js";
+import ApiResponse from 'utils/ApiResponse.js';
 
 const initiateJazzCash = async (
   req: Request,
@@ -8,11 +9,11 @@ const initiateJazzCash = async (
   next: NextFunction
 ) => {
   try {
-    const paymentData = req.body; // Extract data from request body
+    const paymentData = req.body;
     const result = await jazzCashService.initiateJazzCashPayment(paymentData);
-    return res.status(200).json({ success: true, data: result });
+    return res.status(200).json(ApiResponse.success(result));
   } catch (error) {
-    next(error); // Pass error to error handling middleware
+    next(error);
   }
 };
 
