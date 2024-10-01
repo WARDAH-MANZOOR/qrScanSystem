3f0033b92ecfa0602b8daeed0b9ca4d8e7a33915
Muhammad Owais <m.owais1045@gmail.com>
2024-10-01 07:13:24 +0500

    cors added

--- src/app.ts
+++ src/app.ts
@@ -33,7 +33,7 @@ app.set('view engine', 'jade');
 
 // Allow only specific origins
 app.use(cors({
-  origin: ['http://localhost:3000', '*'],
+  origin: ['http://localhost:3005', '*'],
   credentials: true,
 }));
 
