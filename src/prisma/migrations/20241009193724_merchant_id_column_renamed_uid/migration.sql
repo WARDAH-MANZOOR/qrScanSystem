3db0569313c068a6de557d3ddf1efa514214ab5e
Muhammad Owais <m.owais1045@gmail.com>
2024-10-10 00:39:02 +0500

    uid migration done

---
+++ src/prisma/migrations/20241009193724_merchant_id_column_renamed_uid/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `id` on the `Merchant` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Merchant" DROP COLUMN "id",
+ADD COLUMN     "uid" TEXT;
