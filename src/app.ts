0d3c31b60e216b3e96bef0b6fbfb94215e3da7ab
mustafa-tola <mustafatola02@gmail.com>
2024-11-10 11:29:32 +0500

    Updated jazzcash disbursement

--- src/app.ts
+++ src/app.ts
@@ -23,6 +23,7 @@ import completeTransactionRouter from "./routes/transaction/complete.js"
 import adminTransactionRouter from "./routes/user/admin_only.js"
 import { errorHandler } from "./utils/middleware.js";
 import task from "./utils/queue_task.js"
+import { decryptData } from 'utils/enc_dec.js';
 // import { encrypt_payload } from 'utils/enc_dec.js';
 // import backup from 'utils/backup.js';
 
@@ -88,7 +89,7 @@ app.listen(process.env.PORT || 3001, () => {
   console.log(`Server is running on port ${process.env.PORT || 3001}`);
 });
 
-// console.log(encrypt_payload(JSON.stringify({"bankAccountNumber": "01150100189365","bankCode": "49","amount": "1.00","receiverMSISDN": "03059564722","referenceId": "OriID_OFT2451AB23302145709"})))
+console.log(decryptData("814c86e695359028d4cab4ba322d4ba97834c107b918d0f1b5925fa1fb2d990488e89867a0f778f4e36ed3c0970a14dfc02564d5991aa3401f339958c5fbd5657536a646e59d3ecf17feb11e706a0a3051822bced6b1669056e7bd297acdffcc848d9a275339667623461c978782ee53f0afdaf0b4302be81057910f498f2dc87adbaa45c073c1dc5f1e5726de69646fb23010097a840272ea42695250a10f2cd301c3d74bc95c4eb6e31d207685d8f8b9649ad7866476835243ce44007c9326a882c868a583bfb5cf897d19c842e1b76ab960f56e582b44b7008c1a176593a1f4c506453479f077ba65fa4fe84d104210569af551b16df794ba66542ec44bfce1b00683feb56eaac3d54e025298de53af948472f6caaa55ececa1f82122d3d14ed8cee5e96b9c8764b101083757be2fec739205a10dc3da537a91ef08f10e82e04d3b64e8e0d7d2402431f7afa11338dbfbc935239cefef2be7cf0fe3b716e4c2ea22296e402f8d0258fb520ddb09d52a361ad7bec8b4fcb6f4111c771c1f36","mYjC!nc3dibleY3k", "Myin!tv3ctorjCM@"))
 // Example usage
 
 // const encryptedData = encryptWithPublicKey(publicKey, data);
