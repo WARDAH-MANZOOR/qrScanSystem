3c66727a58a6bba63f1c08f3d5d72a14dc49700e
mustafa-tola <mustafatola02@gmail.com>
2024-11-10 20:37:28 +0500

    Updated jazzcash disbursement

--- src/services/paymentGateway/index.ts
+++ src/services/paymentGateway/index.ts
@@ -37,10 +37,10 @@ async function initiateTransaction(token: string) {
   try {
     const id = transactionService.createTransactionId();
     const payload = encryptData({
-      "bankAccountNumber": "01150100189365",
-      "bankCode": "18",
+      "bankAccountNumber": "12345678911112",
+      "bankCode": "49",
       "amount": "1000.00",
-      "receiverMSISDN": "03142304891",
+      "receiverMSISDN": "xxxxxxxxxxx",
       "referenceId": "OriID_"+id
     }
     , "mYjC!nc3dibleY3k", "Myin!tv3ctorjCM@")
