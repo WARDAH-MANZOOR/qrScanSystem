f0df125df32dec68dbc4fcd6de2f28a967cd1fa1
mustafa-tola <mustafatola02@gmail.com>
2024-12-31 12:17:14 +0500

    Add controller and service for transaction creation with validation

--- src/routes/backoffice/backoffice.ts
+++ src/routes/backoffice/backoffice.ts
@@ -1,18 +1,22 @@
 import express from 'express';
 import backOfficeController from 'controller/backoffice/backoffice.js';
+import { isAdmin, isLoggedIn } from 'utils/middleware.js';
 const router = express.Router();
 
 // Define routes
-router.post('/remove-finance/:merchantId', backOfficeController.removeMerchantFinanceData);
+router.post('/remove-finance/:merchantId', [isLoggedIn, isAdmin], backOfficeController.removeMerchantFinanceData);
 
-router.post('/zero-wallet/:merchantId', backOfficeController.zeroMerchantWalletBalance);
+router.post('/zero-wallet/:merchantId', [isLoggedIn, isAdmin], backOfficeController.zeroMerchantWalletBalance);
 
-router.post('/adjust-wallet/:merchantId', backOfficeController.adjustMerchantWalletBalance);
+router.post('/adjust-wallet/:merchantId',  [isLoggedIn, isAdmin], backOfficeController.adjustMerchantWalletBalance);
 
-router.get('/transaction-stats/:merchantId', backOfficeController.checkMerchantTransactionStats);
+router.get('/transaction-stats/:merchantId', [isLoggedIn, isAdmin], backOfficeController.checkMerchantTransactionStats);
 
-router.post('/settle-transactions', backOfficeController.settleTransactions);
+router.post('/settle-transactions', [isLoggedIn, isAdmin], backOfficeController.settleTransactions);
 
-router.post('/settle-all/:merchantId', backOfficeController.settleAllMerchantTransactions);
+router.post('/settle-all/:merchantId', [isLoggedIn, isAdmin], backOfficeController.settleAllMerchantTransactions);
+
+router.post("/dummy-transaction/:merchantId",[isLoggedIn, isAdmin], backOfficeController.createTransactionController)
+
 
 export default router;
