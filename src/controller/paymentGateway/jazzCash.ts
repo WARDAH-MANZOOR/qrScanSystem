2db3aa5c727342eb83fd8210757419e589e260c8
mustafa-tola <mustafatola02@gmail.com>
2025-02-21 17:39:44 +0500

    Fix error handling for missing access token in sandbox disbursement initiation

--- src/controller/paymentGateway/jazzCash.ts
+++ src/controller/paymentGateway/jazzCash.ts
@@ -232,7 +232,7 @@ const initiateSandboxDisbursmentClone = async (req: Request, res: Response, next
     console.log("IBFT Called")
     process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
     const token = await simpleSandboxGetToken(req.params.merchantId);
-    if (token?.access_token) {
+    if (!token?.access_token) {
       res.status(500).json(ApiResponse.error(token))
     }
     const initTransaction = await simpleSandboxinitiateTransactionClone(token?.access_token, req.body, req.params.merchantId);
