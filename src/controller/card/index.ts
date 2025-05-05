5be3be94d4cbf46bdbca07333560c885806bab81
mustafa-tola <mustafatola02@gmail.com>
2025-05-05 16:17:42 +0500

    Implement card payment functionality by adding card controller and service; update routes to include card endpoints for JazzCash merchant retrieval and payment processing.

--- src/controller/card/index.ts
+++ src/controller/card/index.ts
@@ -3,7 +3,28 @@ import { cardService } from "services/index.js";
 import ApiResponse from "utils/ApiResponse.js";
 
 const getJazzCashCardMerchant = async (req: Request, res: Response, next: NextFunction) => {
-    const {merchantId} = req.params;
-    const cardMerchant = await cardService.getJazzCashCardMerchant(merchantId);
-    return res.status(200).json(ApiResponse.success(cardMerchant))
+    try {
+        const { merchantId } = req.params;
+        const cardMerchant = await cardService.getJazzCashCardMerchant(merchantId);
+        res.status(200).json(ApiResponse.success(cardMerchant))
+    }
+    catch (err: any) {
+        next(err)
+    }
+}
+
+const payWithCard = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { merchantId } = req.params;
+        const cardMerchant = await cardService.payWithCard(merchantId, req.body);
+        res.status(200).json(ApiResponse.success(cardMerchant))
+    }
+    catch (err: any) {
+        next(err)
+    }
+}
+
+export default {
+    getJazzCashCardMerchant,
+    payWithCard
 }
