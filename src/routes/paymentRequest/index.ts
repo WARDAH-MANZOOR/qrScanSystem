41a55280c799b2726002416d3962b1eba32f65ed
mustafa-tola <mustafatola02@gmail.com>
2025-08-23 08:17:47 +0500

    Enhance payment processing by adding challengeId handling in OTP verification and improving error reporting for EasyPaisa and PayFast transactions. Update IBAN validation in jazzCash and refine deduction handling in transaction settlements to ensure accurate merchant amounts. Integrate new preRequest function for payment requests to validate phone and location, enhancing overall transaction reliability.

--- src/routes/paymentRequest/index.ts
+++ src/routes/paymentRequest/index.ts
@@ -27,9 +27,9 @@ router.get("/:id", paymentRequestController.getPaymentRequestbyId);
 
 router.get("/", [isLoggedIn], authorize("Invoice Link"), paymentRequestController.getPaymentRequest);
 router.post("/", [isLoggedIn], authorize("Invoice Link"), paymentRequestController.createPaymentRequest);
-// router.post("/:merchantId", paymentRequestController.createPaymentRequestClone);
+router.post("/:merchantId", paymentRequestController.createPaymentRequestClone);
 router.post("/otp/:merchantId", paymentRequestController.createPaymentRequestWithOtp);
-// router.post("/:merchantId", paymentRequestController.createPaymentRequestClone);
+router.post("/:merchantId", paymentRequestController.createPaymentRequestClone);
 // router.post("/new", [isLoggedIn], paymentRequestController.createPaymentRequest);
 router.put(
   "/:paymentRequestId",
